8e43fc595aa6b5f68405044d624890ad021d97c7
perrella1 <pedro.perrella@icloud.com>
2025-02-23 19:45:16 -0300

    feat: add form using React Hook Form, Tailwind CSS, and TypeScript refactor: improve form validation with React Hook Form style: enhance form UI with Tailwind CSS fix: resolve form submission issue

--- hook_form/src/components/Form.tsx
+++ hook_form/src/components/Form.tsx
@@ -1,9 +1,87 @@
+import {useForms} from "../hooks/useForms.tsx"
+import { FormSchema } from "../schemas/formSchema"
 
 const Form = () => {
+    const {register, handleSubmit, errors} = useForms();
+
+    const onSubmit = (data: FormSchema) => {
+        console.log(data);
+    }
   return (
-    <div>
-      Form
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="w-96 bg-white p-4 rounded-md border border-slate-300">
+        <div className="flex flex-col gap-2 mb-2">
+            <label htmlFor="name">Nome</label>
+            <input type="text" id="name" placeholder="Digite o seu nome "{...register("name")} className="h-10 pl-2 rounded-md border border-slate-300"/>
+            {errors.name && (<small className="text-red-500 italic">{errors.name.message}</small>)}
+        </div>
+        <div className="flex flex-col gap-2 mb-2">
+            <label htmlFor="lastname">Sobrenome</label>
+            <input 
+            type="text" 
+            id="lastname" 
+            placeholder="Digite o seu sobrenome "
+            {...register("lastname")} 
+            className="h-10 pl-2 rounded-md border border-slate-300"
+            />
+            {errors.lastname && (<small className="text-red-500 italic">{errors.lastname.message}</small>)}
+        </div>
+        <div className="flex flex-col gap-2 mb-2">
+            <label htmlFor="gender">Gênero</label>
+            <select id="gender" {...register("gender")}a className="h-10 pl-1 rounded-md border border-slate-300">
+                <option value="select">Selecione</option>
+                <option value="male">Masculino</option>
+                <option value="female">Feminino</option>
+                <option value="other">Outro</option>
+            </select>
+            {errors.gender && (<small className="text-red-500 italic">{errors.gender.message}</small>)}
+        </div>
+        <div className="flex flex-col gap-2 mb-2">
+            <label htmlFor="lastname">E-mail</label>
+            <input 
+            type="email" 
+            id="email" 
+            placeholder="Digite o seu e-mail "
+            {...register("email")} 
+            className="h-10 pl-2 rounded-md border border-slate-300"
+            />
+            {errors.email && (<small className="text-red-500 italic">{errors.email.message}</small>)}
+        </div>
+        <div className="flex flex-col gap-2 mb-2">
+            <label htmlFor="password">Senha</label>
+            <input 
+            type="password" 
+            id="password" 
+            placeholder="Digite sua senha"
+            {...register("password")} 
+            className="h-10 pl-2 rounded-md border border-slate-300"
+            />
+            {errors.password && (<small className="text-red-500 italic">{errors.password.message}</small>)}
+        </div>
+        <div className="flex flex-col gap-2 mb-2">
+            <label htmlFor="confirmpassword">Confirmação de senha</label>
+            <input 
+            type="password" 
+            id="confirmpassword" 
+            placeholder="Confirme sua senha"
+            {...register("confirmpassword")} 
+            className="h-10 pl-2 rounded-md border border-slate-300"
+            />
+            {errors.confirmpassword && (<small className="text-red-500 italic">{errors.confirmpassword.message}</small>)}
+        </div>
+        <div className="flex flex-col gap-2 mb-2">
+            <div className="flex gap-2">
+            <input 
+            type="checkbox" 
+            id="agree" 
+            placeholder="Confirme sua senha"
+            {...register("agree")} 
+            />
+            <label htmlFor="agree">Concordo com os termos</label>
+            </div>
+            {errors.agree && (<small className="text-red-500 italic">{errors.agree.message}</small>)}
+        </div>
+        <button type="submit" className="w-full h-11 bg-cyan-400 rounded-md my-2 hover:bg-cyan-500 transition-all">Cadastrar</button>
+    </form>
   )
 }
 
